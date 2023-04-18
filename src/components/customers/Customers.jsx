9d12766ae70c7d9a5f5010d1fd644b5b875de3b6
Qoryogdiyev <asadbekqoryogdiyev7@gmail.com>
2023-04-19 01:38:08 +0500

    add new section

---
+++ src/components/customers/Customers.jsx
@@ -0,0 +1,11 @@
+import "../customers/customers.scss";
+
+function Customers() {
+  return (
+    <section className="customers">
+      <div className="customers__container"></div>
+    </section>
+  );
+}
+
+export default Customers;
